6907f79cab0fa300c9e3839d4ea755b55c712779
Katina T <kthongvong16@gmail.com>
2017-02-11 11:31:07 -0800

    clean up

--- Feb8Test2/src/org/usfirst/frc/team5938/robot/Robot.java
+++ Feb8Test2/src/org/usfirst/frc/team5938/robot/Robot.java
@@ -21,23 +21,14 @@ import edu.wpi.first.wpilibj.VictorSP;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot implements PIDOutput{
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
+public class Robot extends IterativeRobot{
+	
+	
 	SendableChooser<String> chooser = new SendableChooser<>();
 	RobotDrive myRobot;
 	Joystick mainStick;
 	AHRS ahrs;
-	VictorSP motorZero;
-	VictorSP motorOne; 
-	int autoLoopCounter;
-	 PIDController turnController;
-	 static final double kP = 0.03;
-	 static final double kI = 0.00;
-	 static final double kD = 0.00;
-	 static final double kF = 0.00;
-	 static final double kToleranceDegrees = 2.0f;
+	
 	 boolean buttonPress = true;
 	
 	
@@ -65,11 +56,7 @@ myRobot = new RobotDrive(0, 1);
 	          DriverStation.reportError("Error instantiating navX-MXP:  " + ex.getMessage(), true);
 
 	      }
-		 turnController = new PIDController(kP, kI, kD, kF, ahrs, this);
-	        turnController.setInputRange(-180.0f,  180.0f);
-	        turnController.setOutputRange(-1.0, 1.0);
-	        turnController.setAbsoluteTolerance(kToleranceDegrees);
-	        turnController.setContinuous(true);
+		
 
 	  }
 	
@@ -143,95 +130,14 @@ myRobot = new RobotDrive(0, 1);
 
 	          /* magnetometer calibration to be useful)                                   */
 
-	          
-
-	          SmartDashboard.putNumber(   "IMU_CompassHeading",   ahrs.getCompassHeading());
+	         
 
 	          
-
-	          /* Display 9-axis Heading (requires magnetometer calibration to be useful)  */
-
-	          SmartDashboard.putNumber(   "IMU_FusedHeading",     ahrs.getFusedHeading());
-
-
-
-	          /* These functions are compatible w/the WPI Gyro Class, providing a simple  */
-
-	          /* path for upgrading from the Kit-of-Parts gyro to the navx-MXP            */
-
-	          
-
-	          SmartDashboard.putNumber(   "IMU_TotalYaw",         ahrs.getAngle());
-
-	          SmartDashboard.putNumber(   "IMU_YawRateDPS",       ahrs.getRate());
-
-
-
-	          /* Display Processed Acceleration Data (Linear Acceleration, Motion Detect) */
-
-	          
-
-	          SmartDashboard.putNumber(   "IMU_Accel_X",          ahrs.getWorldLinearAccelX());
-
-	          SmartDashboard.putNumber(   "IMU_Accel_Y",          ahrs.getWorldLinearAccelY());
 
 	          SmartDashboard.putBoolean(  "IMU_IsMoving",         ahrs.isMoving());
 
 	          SmartDashboard.putBoolean(  "IMU_IsRotating",       ahrs.isRotating());
 
-
-
-	          /* Display estimates of velocity/displacement.  Note that these values are  */
-
-	          /* not expected to be accurate enough for estimating robot position on a    */
-
-	          /* FIRST FRC Robotics Field, due to accelerometer noise and the compounding */
-
-	          /* of these errors due to single (velocity) integration and especially      */
-
-	          /* double (displacement) integration.                                       */
-
-	          
-
-	          SmartDashboard.putNumber(   "Velocity_X",           ahrs.getVelocityX());
-
-	          SmartDashboard.putNumber(   "Velocity_Y",           ahrs.getVelocityY());
-
-	          SmartDashboard.putNumber(   "Displacement_X",       ahrs.getDisplacementX());
-
-	          SmartDashboard.putNumber(   "Displacement_Y",       ahrs.getDisplacementY());
-
-	          
-
-	          /* Display Raw Gyro/Accelerometer/Magnetometer Values                       */
-
-	          /* NOTE:  These values are not normally necessary, but are made available   */
-
-	          /* for advanced users.  Before using this data, please consider whether     */
-
-	          /* the processed data (see above) will suit your needs.                     */
-
-	          
-
-	          SmartDashboard.putNumber(   "RawGyro_X",            ahrs.getRawGyroX());
-
-	          SmartDashboard.putNumber(   "RawGyro_Y",            ahrs.getRawGyroY());
-
-	          SmartDashboard.putNumber(   "RawGyro_Z",            ahrs.getRawGyroZ());
-
-	          SmartDashboard.putNumber(   "RawAccel_X",           ahrs.getRawAccelX());
-
-	          SmartDashboard.putNumber(   "RawAccel_Y",           ahrs.getRawAccelY());
-
-	          SmartDashboard.putNumber(   "RawAccel_Z",           ahrs.getRawAccelZ());
-
-	          SmartDashboard.putNumber(   "RawMag_X",             ahrs.getRawMagX());
-
-	          SmartDashboard.putNumber(   "RawMag_Y",             ahrs.getRawMagY());
-
-	          SmartDashboard.putNumber(   "RawMag_Z",             ahrs.getRawMagZ());
-
-	          SmartDashboard.putNumber(   "IMU_Temp_C",           ahrs.getTempC());
 
 	          
 
@@ -265,20 +171,6 @@ myRobot = new RobotDrive(0, 1);
 
 	          SmartDashboard.putNumber(   "QuaternionW",          ahrs.getQuaternionW());
 
-	          SmartDashboard.putNumber(   "QuaternionX",          ahrs.getQuaternionX());
-
-	          SmartDashboard.putNumber(   "QuaternionY",          ahrs.getQuaternionY());
-
-	          SmartDashboard.putNumber(   "QuaternionZ",          ahrs.getQuaternionZ());
-
-	          
-
-	          /* Connectivity Debugging Support                                           */
-
-	          SmartDashboard.putNumber(   "IMU_Byte_Count",       ahrs.getByteCount());
-
-	          SmartDashboard.putNumber(   "IMU_Update_Count",     ahrs.getUpdateCount());
-	          
 	          
 	      }
 		}
@@ -293,10 +185,7 @@ myRobot = new RobotDrive(0, 1);
 
 
 	@Override
-	public void pidWrite(double output) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 	
 	public double testNum(){
 		double x;
